fd2b831ba74b72dcf22e42ac4e442fbaf88605a6
satya2343 <satya24@student.uns.ac.id>
2017-04-29 07:42:11 +0700

    las

--- controllers/MonitorController.php
+++ controllers/MonitorController.php
@@ -200,4 +200,5 @@ class MonitorController extends Controller
 
         return $isi;
     }
+
 }
--- views/site/index.php
+++ views/site/index.php
@@ -7,45 +7,44 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Proyek Kreatif 4</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <h2   >Sistem Parkir Cerdas</h2>
+        <p class="lead">Bima Tri Prasetya I0714007</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="#">PARKIR</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Apa itu</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Sistem Parkir Cerdas adalah sistem parkir modern yang mampu membantu orang untuk
+                membantu orang mencari tempat parkir yang kosong.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <!-- <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p> -->
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Latar Belakang</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Orang - orang membutuhkan waktu secara penuh. Maka dari itu semua orang tidak mau
+                membuang waktunya untuk hal sepele seperti mencari tempat parir yang kosong</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <!-- <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p> -->
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Cara Kerja Sistem</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>1. Orang memesan lokasi parkir dengan menekan tombol
+                </p>
+                <p>2. Pengguna mendapatkan lokasi parkir yang sudah diolah oleh sistem
+                </p>
+                <p>3. Pengguna Menempati lokasi parkir yang sudah ditentukan
+                </p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <!-- <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p> -->
             </div>
         </div>
 
